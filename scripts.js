b1c1f68d2abc5e59085dbf30b18dedc46c3227d2
Mohamed El Amine DADDOU <daddoumohamed31@gmail.com>
2017-02-15 22:23:36 +0100

    Basic UCondidate Picker

---
+++ scripts.js
@@ -0,0 +1,58 @@
+let UTimer = function() {
+    
+    $level1  = document.getElementById('l1')
+    $level2  = document.getElementById('l2')
+    $level3  = document.getElementById('l3')
+
+    
+    let idInterval = null
+    let idTimeout  = null
+    
+    this.random = {
+        get random() {
+            return random;
+        },
+        set random(value) {
+            
+            random = value
+            
+            let l1 = Math.floor(random / 100)
+            let l2 = Math.floor((random % 100) / 10)
+            let l3 = random % 10
+            
+            $level1.innerText = l1
+            $level2.innerText = l2
+            $level3.innerText = l3
+        }
+    }
+
+    let self = this
+    
+    this.reset = function() {
+        
+        clearInterval(idInterval)
+        idInterval = null
+        
+        self.random.random = 0
+    }
+    
+    
+    this.reset()
+    
+    this.start = function(min, max) {
+        
+        this.reset()
+        
+        idInterval = setInterval(function() {
+            self.random.random = Math.floor(Math.random() * (max - min + 1)) + min
+        }, 10)
+        
+        idTimeout = setTimeout(function() {
+            clearInterval(idInterval)
+        }, 5000)
+    }    
+    
+    return this;
+}
+
+let timer = new UTimer()
